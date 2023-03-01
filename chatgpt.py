27d0decdf5c718c23d7e0eecc34aa7820642530a
Kush Shah <32845063+kush5683@users.noreply.github.com>
2023-03-01 16:43:11 -0500

    changed chaptgpt to use the new chatgpt api

--- chatgpt.py
+++ chatgpt.py
@@ -1,25 +1,19 @@
 import openai
 
 class Chatbot:
-    def __init__(self, model="text-davinci-003"):
-        self.currentThread = """The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
-                                Human: Hello, who are you?
-                                AI: I am an AI created by OpenAI. How can I help you today?
-                                Human: 
-                                """
+    def __init__(self, model="gpt-3.5-turbo"):
         self.model = model
+        self.messages = [
+        {"role": "system", "content":"You are a helpful assistant. You take on any behavior asked of you"},
+        {"role": "user", "content": f"{prompt}"}
+        ]
     
     def send(self, message):
-        self.currentThread += f"Human: {message}\n AI:"
-        response = openai.Completion.create(
-            model=self.model,
-            prompt=self.currentThread,
-            temperature=0.9,
-            max_tokens=1000,
-            top_p=1,
-            frequency_penalty=0.0,
-            presence_penalty=0.6,
-            stop=[" Human:", " AI:"]
+        messages.append({"role": "user", "content": f"{message}"})
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages
         )
-        self.currentThread += f"{response['choices'][0]['text']}"
-        return response['choices'][0]['text']
+        message_content = response["choices"][0]["message"]['content']
+        self.messages.append({"role": "assistant", "content": message_content})
+        return message_content
